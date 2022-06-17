9c196c16a4fa085841db3124a13d0a7c02687fd5
Timm Knape <timm@knp.de>
2022-06-17 12:29:29 +0200

    Initial Commit

---
+++ Shared/NachSpielApp.swift
@@ -0,0 +1,17 @@
+//
+//  NachSpielApp.swift
+//  Shared
+//
+//  Created by Timm Knape on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct NachSpielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
